5ca086eafd331a5b85d6230555a6868aa41c3e9b
Eric Secules <esecules@gmail.com>
2015-09-27 00:02:21 -0700

    Update install.sh
    always remove root crontab when done with it.

--- install.sh
+++ install.sh
@@ -19,5 +19,6 @@ else
     # Run every 5 minutes
     echo "*/5 * * * * $INSTALL_PATH/suspend_check >> /var/log/suspend_log.txt 2>&1 && $INSTALL_PATHsuspend_script >> /var/log/suspend_log.txt >/dev/null 2>&1" >> root_cron
     crontab root_cron
-    rm root_cron
 fi
+  rm root_cron
+  
